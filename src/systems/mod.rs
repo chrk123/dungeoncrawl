840fe0f94da6cf1c1406671214eb595a4cd9f6f1
Christoph Kick <kick.christoph@gmail.com>
2022-12-21 17:01:37 +0100

    Introduce MovingRandomly component

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -2,14 +2,17 @@ mod collisions;
 mod entity_renderer;
 mod map_renderer;
 mod player_input;
+mod random_move;
 
 use crate::prelude::*;
 
 pub fn build_scheduler() -> Schedule {
     Schedule::builder()
-        .add_system(map_renderer::map_render_system())
         .add_system(player_input::player_input_system())
-        .add_system(entity_renderer::entity_render_system())
         .add_system(collisions::collisions_system())
+        .flush()
+        .add_system(map_renderer::map_render_system())
+        .add_system(entity_renderer::entity_render_system())
+        .add_system(random_move::random_move_system())
         .build()
 }

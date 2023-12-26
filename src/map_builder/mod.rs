5c96c3cfb30493d9e9aba7e5b571431153f5f5ab
Christoph Kick <kick.christoph@gmail.com>
2023-12-26 14:38:23 +0100

    Chose map architect randomly

--- src/map_builder/mod.rs
+++ src/map_builder/mod.rs
@@ -1,14 +1,15 @@
 use crate::prelude::*;
 mod automata;
+mod drunkard;
 mod empty;
 mod rooms;
-mod drunkard;
 
 //use empty::EmptyArchitect;
 //use rooms::RoomsArchitect;
 //use automata::CellularAutomataArchitect;
 use drunkard::DrunkardsWalkArchitect;
 
+use self::{automata::CellularAutomataArchitect, rooms::RoomsArchitect};
 
 trait MapArchitect {
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder;
@@ -27,8 +28,11 @@ pub struct MapBuilder {
 
 impl MapBuilder {
     pub fn new(rng: &mut RandomNumberGenerator) -> Self {
-        // let mut architect = EmptyArchitect{};
-        let mut architect = DrunkardsWalkArchitect {};
+        let mut architect: Box<dyn MapArchitect> = match rng.range(0, 3) {
+            0 => Box::new(DrunkardsWalkArchitect {}),
+            1 => Box::new(RoomsArchitect {}),
+            _ => Box::new(CellularAutomataArchitect {}),
+        };
         architect.new(rng)
     }
 
